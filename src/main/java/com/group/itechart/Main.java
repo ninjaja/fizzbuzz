7c7a7f3e303a8bf09fb33ef5a5e3e850479d4293
Dmitry.Matrizaev <c-dmatrizaev@neogov.net>
2021-11-11 17:17:08 +0300

    added method for input map, simple validation and logging

--- src/main/java/com/group/itechart/Main.java
+++ src/main/java/com/group/itechart/Main.java
@@ -1,13 +1,28 @@
 package com.group.itechart;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author dmitry.matrizaev
  */
 
 public class Main {
 
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+
+
         SimpleFizzerBuzzer fizzerBuzzer = new SimpleFizzerBuzzer();
-        System.out.println(fizzerBuzzer.doBuzzAndFuzz(1, 100));
+        System.out.println(fizzerBuzzer.doFizzAndBuzzFromRange(1, 100));
+
+        Map<Integer, String> inputMap = new HashMap<>();
+        inputMap.put(3, "Fizz");
+        inputMap.put(5, "Buzz");
+        System.out.println(fizzerBuzzer.doFizzAndBuzzFromMapAndRange(inputMap, 1, 100));
     }
 }
--- src/main/java/com/group/itechart/SimpleFizzerBuzzer.java
+++ src/main/java/com/group/itechart/SimpleFizzerBuzzer.java
@@ -1,23 +1,80 @@
 package com.group.itechart;
 
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author dmitry.matrizaev
  */
 
 public class SimpleFizzerBuzzer {
 
-    public StringBuilder doBuzzAndFuzz(int from, int to) {
+    public static final Logger LOGGER = LogManager.getLogger(SimpleFizzerBuzzer.class);
+
+    public StringBuilder doFizzAndBuzzFromRange(int from, int to) {
         StringBuilder result = new StringBuilder();
-        for (int i = from; i <= to; i++) {
-            if (i % 3 == 0) {
-                result.append("Fizz");
-            } else if (i % 5 == 0) {
-                result.append("Buzz");
-            } else {
-                result.append(i);
+        if (validateInputRange(from, to)) {
+            for (int i = from; i <= to; i++) {
+                if (i % 3 == 0) {
+                    result.append("Fizz");
+                } else if (i % 5 == 0) {
+                    result.append("Buzz");
+                } else {
+                    result.append(i);
+                }
+                result.append(" ");
             }
-            result.append(" ");
         }
         return result;
+    }
+
+    public StringBuilder doFizzAndBuzzFromMapAndRange(Map<Integer, String> inputMap, int from, int to) {
+        StringBuilder result = new StringBuilder();
+        if (validateInputRange(from, to) && validateInputMap(inputMap)) {
+            for (int i = from; i <= to; i++) {
+                String valueToAppend = null;
+                for (Map.Entry<Integer, String> entry : inputMap.entrySet()) {
+                    if (i % entry.getKey() == 0) {
+                        valueToAppend = entry.getValue();
+                    }
+                }
+                if (Objects.isNull(valueToAppend)) {
+                    valueToAppend = String.valueOf(i);
+                }
+                result.append(valueToAppend).append(" ");
+            }
+        }
+        return result;
+    }
+
+    private boolean validateInputRange(int from, int to) {
+        boolean isInputValid;
+        if (from >= 0 && to >= 0 && from < to) {
+            isInputValid = true;
+        } else {
+            LOGGER.warn("Please enter correct input range values");
+            isInputValid = false;
+        }
+        return isInputValid;
+    }
+
+    private boolean validateInputMap(Map<Integer, String> map) {
+        boolean isMapValid = true;
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            if (entry.getKey() < 0) {
+                LOGGER.warn("Input map keys should be more than 0");
+                isMapValid = false;
+            }
+            if (StringUtils.isEmpty(entry.getValue())) {
+                LOGGER.warn("Input map values should not be empty or null");
+                isMapValid = false;
+            }
+        }
+        return isMapValid;
     }
 }
